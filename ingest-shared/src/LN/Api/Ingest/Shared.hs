81b0289c604d81fc22aab5bbd2108e28241b1232
Andrew Darqui <andrew.darqui@gmail.com>
2017-04-10 00:33:41 -0400

    some shared stuff .. eventually for sharing code between these tools

---
+++ ingest-shared/src/LN/Api/Ingest/Shared.hs
@@ -0,0 +1,7 @@
+module LN.Api.Ingest.Shared (
+  module A
+) where
+
+
+
+import           LN.Api.Ingest.Shared.Internal as A
---
+++ ingest-shared/src/LN/Api/Ingest/Shared/Internal.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
+module LN.Api.Ingest.Shared.Internal (
+) where
+
+
+
+import           Control.Monad
+import           Control.Monad.IO.Class     (MonadIO, liftIO)
+import           Data.Aeson
+import           Data.ByteString            (ByteString)
+import qualified Data.ByteString.Lazy       as B
+import           Data.Int                   (Int64)
+import           Data.Maybe                 (catMaybes)
+import           Data.Monoid                ((<>))
+import           Data.String.Conversions    (cs)
+import           Data.Text
+import qualified Data.Text                  as Text
+import           Haskell.Api.Helpers        (SpecificApiOptions, defaultSpecificApiOptions)
+import           Haskell.Api.Helpers.Shared (ApiError (..), ApiOptions (..),
+                                             runWith)
+
+import           LN.Api
+
+
+
+api api_key api_url fn = do
+  lr <- runWith fn defaultApiOpts { apiKey = Just api_key, apiUrl = api_url }
+  print lr
+
+
+
+defaultApiOpts :: ApiOptions SpecificApiOptions
+defaultApiOpts = ApiOptions {
+  apiUrl         = "http://dev.adarq.org",
+  apiPrefix      = "api",
+  apiKey         = Nothing,
+  apiKeyHeader   = Just "x-api-authorization",
+  apiOptions     = defaultSpecificApiOptions,
+  apiDebug       = False
+}
